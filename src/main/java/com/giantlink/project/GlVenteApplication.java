bd57f59cfd77117a8941115f65696ea9100e47a9
zomarex17 <brahim-arfa@hotmail.com>
2022-07-29 09:18:36 +0100

    added spring security + filter

--- src/main/java/com/giantlink/project/GlVenteApplication.java
+++ src/main/java/com/giantlink/project/GlVenteApplication.java
@@ -1,13 +1,19 @@
 package com.giantlink.project;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GlVenteApplication {
+public class GlVenteApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlVenteApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Bienvenue dans le portail de vente");
 	}
 
 }
